948b42c0ce38542b6bbdc972d6cfe50be7ad2057
markdenardo <mark.denardo@gmail.com>
2020-06-27 00:09:38 -0400

    Updates

---
+++ precache-manifest.452de6989079414a5c712d1033b7436f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9eefb4b2cab681c6e4b2855ea07aa27",
+    "url": "/tt-app/index.html"
+  },
+  {
+    "revision": "d0477c69f5961a390fbf",
+    "url": "/tt-app/static/css/main.d2396d0b.chunk.css"
+  },
+  {
+    "revision": "edc6beb6b6ec9a936901",
+    "url": "/tt-app/static/js/2.242d9a47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tt-app/static/js/2.242d9a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0477c69f5961a390fbf",
+    "url": "/tt-app/static/js/main.8b62661a.chunk.js"
+  },
+  {
+    "revision": "f0d4839112706bc82edf",
+    "url": "/tt-app/static/js/runtime-main.8855b35f.js"
+  }
+]);
