31cd65c4ec92dfb432b32e5b2360709494eac9ca
markdenardo <mark.denardo@gmail.com>
2020-06-27 19:41:50 -0400

    routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,18 +11,18 @@ import Home from '../components/Home'
 import Trailers from '../components/Trailers'
 import Movies from '../components/Movies'
 
-function App (){
+const App = (props)=> {
   return (
     <Router>
     <div className="App">
       <header className="app-header">
           {/* <Route exact path={process.env.PUBLIC_URL + '/'} component={App}/> */}
       <NavBar />
-          <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
+          <Route exact path="/" component={Home} />
         {/* <Route exact path="/home" component={Home} /> */}
-          <Route exact path={process.env.PUBLIC_URL + '/trailers'} component={Trailers} />
+          <Route exact path="/trailers" component={Trailers} />
         {/* <Route exact path="/trailers" component={Trailers} /> */}
-          <Route exact path={process.env.PUBLIC_URL + '/movies'} component={Movies} />
+          <Route exact path="/movies" component={Movies} />
           {/* <Route exact path="/movies" component={Movies} /> */}
       </header>
     </div>
